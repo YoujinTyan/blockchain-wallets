473bf4e816217d5758455be4aceb2b6ce768946a
yt <tyanbz@gmail.com>
2024-01-10 16:56:44 +0600

    спрятал url-адреса в окружение

--- src/services/api.js
+++ src/services/api.js
@@ -3,8 +3,15 @@ import { setNotFound, setPosts } from "../store/reducers/post_reducer";
 import { store } from "../store/store";
 
 export const fetchData = async (page, limit, searchQuery) => {
+  const baseURL = process.env.REACT_APP_BASE_URL;
+  const paramPages = process.env.REACT_APP_PAGE;
+  const paramLimit = process.env.REACT_APP_LIMIT;
+  const paramTitles = process.env.REACT_APP_TITLES;
+  const postsURL = `${baseURL}posts?${paramPages}${page}${paramLimit}${limit}`;
+  const searchQueryURL = `${baseURL}posts?${paramPages}${page}${paramLimit}${limit}${paramTitles}${searchQuery}`;
+
   if (searchQuery.length > 0) {  // если есть запрос, то ищем
-    await fetch(postsURLByTitle(page, limit, searchQuery))
+    await fetch(searchQueryURL)
       .then((response) => response.json())
       .then((posts) => {
         store.dispatch(setPosts(posts));
@@ -13,7 +20,7 @@ export const fetchData = async (page, limit, searchQuery) => {
         };
       });
   } else { // если запроса нет, то запрашиваем посты по страницам и лимитам
-    fetch(postsURL(page, limit))
+    fetch(postsURL)
       .then((response) => response.json())
       .then((posts) => { // и отпрваляем в стор
         store.dispatch(setPosts(posts));
