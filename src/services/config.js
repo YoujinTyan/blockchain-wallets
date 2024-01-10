b010b30ce9896a0255d2236b580277465f30518e
yt <tyanbz@gmail.com>
2024-01-10 16:57:11 +0600

    удалил файл со старыми адресами

--- src/services/config.js
+++
@@ -1,5 +0,0 @@
-export const postsURL = (page, limit) =>
-  `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${limit}`;
-
-export const postsURLByTitle = (page, limit, searchQuery) =>
-  `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${limit}&title_like=${searchQuery}`;
