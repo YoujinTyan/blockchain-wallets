19e017fd0e4ce51480a8f5ed261ff0361cfcaa9f
Andrew Serbin <serbinadrian@outlook.com>
2024-01-31 13:09:24 +0300

    Fixed pages naming

--- src/pages/NamePage/index.jsx
+++
@@ -1,9 +0,0 @@
-import GeneralLayout from "../../layouts/GeneralLayout";
-
-export default function Children() {
-  return (
-    <GeneralLayout>
-      <p>children</p>
-    </GeneralLayout>
-  );
-};
---
+++ src/pages/SettingsPage/index.jsx
@@ -0,0 +1,9 @@
+import GeneralLayout from "../../layouts/GeneralLayout";
+
+export default function SettingsPage() {
+    return (
+        <GeneralLayout>
+            <p>Settings</p>
+        </GeneralLayout>
+    );
+}
